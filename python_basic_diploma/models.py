217c293c98ff748ef1ecc6b0eb304bfb1578590e
Arseny <dyd_ars15@mail.ru>
2022-05-26 12:22:38 +0300

    edit db connect

--- python_basic_diploma/models.py
+++ python_basic_diploma/models.py
@@ -1,7 +1,11 @@
 from peewee import Model, CharField, PostgresqlDatabase
+from playhouse.db_url import connect
+import os
 
-db = PostgresqlDatabase(database='telegram_bot_history', host="guarded-oasis-38067.herokuapp.com", port=5432,
-                        user='postgres', password="Arseny_20")
+db = connect(os.environ.get('DATABASE_URL'))
+
+# db = PostgresqlDatabase(database='telegram_bot_history', host="guarded-oasis-38067.herokuapp.com", port=5432,
+#                         user='postgres', password="Arseny_20")
 
 
 class BaseModel(Model):
